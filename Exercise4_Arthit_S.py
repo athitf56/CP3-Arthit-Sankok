af4eeb9988a6787d6c0f5ae218f7ddf3b1564a2e
athitf56 <72957776+athitf56@users.noreply.github.com>
2021-01-27 10:06:32 +0700

    Add files via upload

---
+++ Exercise4_Arthit_S.py
@@ -0,0 +1,10 @@
+Foundation_EnglishAge = 85.5
+General_Business = 79.5
+Introduction_to_Computer_Systems =80.5
+Computer_Programming = 90
+
+print("--- Your Score ---")
+print("Foundation_EnglishAge","คะแนนที่ได้" ,Foundation_EnglishAge)
+print("General_Business","คะแนนที่ได้" ,General_Business)
+print("Introduction_to_Computer_Systems","คะแนนที่ได้" ,Introduction_to_Computer_Systems)
+print("Computer_Programming","คะแนนที่ได้" ,Computer_Programming)
